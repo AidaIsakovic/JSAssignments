eeadea978bffc437e81a957745fc806c01e67f2f
AidaIsakovic <aidisa664@ihmeducation.se>
2024-09-12 16:03:58 +0200

    Create app3.js

---
+++ P5Operators/js/app3.js
@@ -0,0 +1,63 @@
+//let x = 5 + 5; // x = 10//
+//let y = x + 5; // y = 15 //
+//let z = y + y; // z = 30 //
+//console.log(z + z); // 60 //
+
+//let firstNumber = 7;
+//let secondNumber = 2;
+//let seven=7;
+//let answer = 0
+// 1 + 2 = 3
+//answer = firstNumber + secondNumber;
+//console.log(answer);
+
+//answer = firstNumber - secondNumber;
+//console.log(answer);
+
+//answer = firstNumber * secondNumber;
+//console.log(answer);
+//answer = firstNumber / secondNumber;
+//console.log(answer);
+
+//answer = firstNumber % secondNumber;
+//console.log(answer);
+
+//let gold = 50;
+//gold + 10;
+//console.log(gold);
+
+//gold = gold + 10;
+//console.log(gold);
+
+//gold += 10;
+//console.log(gold); // 70
+
+//gold *= 10;
+//console.log(gold); // 700
+
+//gold /= 10;
+//console.log(gold); // 70
+
+//gold -= 10;
+//console.log(gold); // 60
+
+//gold %= 2;
+//console.log(gold); // 0
+
+//let numberOfStudents = 0;
+
+//numberOfStudents += 1;
+//console.log(numberOfStudents); // 1
+
+//numberOfStudents++;
+//console.log(numberOfStudents); // 2
+
+//++numberOfStudents;
+//console.log(numberOfStudents); // 3
+
+//console.log(numberOfStudents++); // 3 why?
+//console.log(numberOfStudents); // 4 why???
+//console.log(++numberOfStudents); // 5
+
+
+
