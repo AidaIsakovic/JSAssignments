16e8bf3b5dbc3472de2322a65c413a0bbf844f91
AidaIsakovic <aidisa664@ihmeducation.se>
2024-09-12 10:53:46 +0200

    create new project

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -1,3 +1,8 @@
-console.log("Hello World");
-console.log("My name is Aida");
+let hoursInClass = 5;
+let hoursOfSleep   = 8;
+console.log(hoursInClass);
+console.log(hoursOfSleep);
+console.log(5+8);
+let total = 0
+console.log(total)
 
