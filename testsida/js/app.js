540d77d25fbf0fdbdc009ee66c8c550a2040657f
AidaIsakovic <aidisa664@ihmeducation.se>
2024-09-19 11:25:42 +0200

    add button that increases number

--- testsida/js/app.js
+++ testsida/js/app.js
@@ -4,24 +4,31 @@ const textField0 = document.getElementById("text0");
 
 //BUTTONS
 const increaseScoreButton = document.getElementById("button0");
+
 //Internal Variables
 let score = 0;
 
 // process (what is going to happen in what sequence?)
 increaseScoreButton.addEventListener("click", () => {
   increaseScoreByOne();
-
+  updateScoreText();
+  checkScoreForSeven();
 });
 
-
 // controllers
-
 function increaseScoreByOne() {
   score++;
-  updateScoreText(score);
+}
+function checkScoreForSeven() {
+  if (score >= 7) {
+    changeScoreTextColorToGreen();
+  }
 }
 
-// The view
-function updateScoreText (newText) {
-  textField0.innerHTML = newText;
+// view
+function updateScoreText () {
+  textField0.innerHTML ="Your Score Is:" + score;
+}
+function changeScoreTextColorToGreen() {
+  textField0.style.color = "green";
 }
